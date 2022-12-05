1de9893d25f7a41c6b4de1f99e5f4560434067d5
RafaelSaavedra <rafaelsaavedra2005@hotmail.com>
2022-12-05 12:46:22 -0600

    Muestra resultados con alerta

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+
 const loadInitialTemplate = () => {
     const template = `
     <h1> The Rock-paper-scissors-spock-lizard API</h1>
@@ -10,9 +11,16 @@ const loadInitialTemplate = () => {
             <label>Option</label>
             <Input name = "option" />
         </div>
+        <div>
+            <label>Result</label>
+            <Input name = "result" />
+        </div>
+
         <button type ="submit">Enviar</button>
+
     </form>
     <ul id="user-list"></ul>
+    <ul id="result"></ul>
     `
     const body = document.getElementsByTagName('body')[0]
     body.innerHTML = template
@@ -24,7 +32,7 @@ const getUsers = async () => {
     //console.log(users) AQUI OBTIENES NOMBRE, OPCIÓN Y ID YA REGISTRADO EN MONGO
     const template = user => `
     <li>
-        ${user.name} ${user.option} <button data-id="${user._id}">Eliminar</button>
+        ${user.name} ${user.option} ${user.result}<button data-id="${user._id}">Eliminar</button>
     </li>
     `
 
@@ -49,8 +57,49 @@ const addFormListener = () => {
     const formData = new FormData(userForm) 
     //console.log(formData.get('name')) AQUI OBTIENES NOMBRE
     //console.log(formData.get('option')) AQUI ONTIENES OPCION
+
+let player = formData.get('option')
+//let computer = formData.get('result')
+
+let rand = Math.floor(Math.random()*11)
+if(rand < 3) computer = 'rock'
+    else if( rand <5) computer = 'paper'
+    else if( rand <7) computer = 'scissors'
+    else if( rand <9) computer = 'spock'
+    else if( rand <11) computer = 'lizzard'
+
+
+if(player =='rock' && (computer == 'paper' || computer == 'spock')
+||player == 'paper' && (computer == 'scissors' || computer == 'lizzard')
+||player == 'scissors' && (computer == 'rock' || computer == 'spock')
+||player == 'spock' && (computer == 'paper' || computer == 'lizzard')
+||player == 'lizzard' && (computer == 'scissors'|| computer == 'rock'))
+
+{
+    alert ('player :' + player +', Computer :' + computer +", Result : Computer wins.")
+
+}
+
+else if(player == computer)
+
+{alert ('player :' + player +', Computer :' + computer +", Result : Draw, try again.")}
+
+else if(player !== ('rock' || 'paper' ||'scissors' || 'spock' || 'lizzard'))
+{
+
+alert( 'no MAMEIS : no existe esa opción')
+
+}
+
+else
+
+{alert ('player :' + player +', Computer :' + computer +", Result : Player wins.")}
+    
+   
+
+
     const data = Object.fromEntries(formData.entries())
-    //console.log(data) OBJETO JSON CON NOMBRE Y OPCION
+    console.log(data) //OBJETO JSON CON NOMBRE Y OPCION
     await fetch('/users', {
        method : 'POST',
        body: JSON.stringify(data),
